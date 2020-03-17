c78856e69ff210a19a61e484f31c37ac06d419ef
Kimberly Aviles <01010110.v@gmail.com>
2020-03-17 18:51:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  # array.shift
-  # array.delete_at(0)
-  array.slice(0)
+  array.shift
+# array.delete_at(0)
+# array.slice(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.delete_at(index_number)
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
